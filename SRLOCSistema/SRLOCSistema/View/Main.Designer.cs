1de98c95cdf310047a67c339d80f47a31b0d7316
João Mago <joao.mago@drlavatudo.com>
2018-08-02 12:58:38 -0300

    Criando interface inicial

--- SRLOCSistema/SRLOCSistema/View/Main.Designer.cs
+++ SRLOCSistema/SRLOCSistema/View/Main.Designer.cs
@@ -30,53 +30,132 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.CadastrarItem = new System.Windows.Forms.Button();
+            this.CadastrarComodo = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(899, 322);
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(824, 286);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "Gerar Excel";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.CadastrarComodo);
+            this.panel1.Controls.Add(this.CadastrarItem);
             this.panel1.Location = new System.Drawing.Point(12, 364);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(962, 100);
             this.panel1.TabIndex = 1;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(313, 293);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(331, 65);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(313, 293);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // CadastrarItem
+            // 
+            this.CadastrarItem.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.CadastrarItem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CadastrarItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CadastrarItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CadastrarItem.Location = new System.Drawing.Point(13, 15);
+            this.CadastrarItem.Name = "CadastrarItem";
+            this.CadastrarItem.Size = new System.Drawing.Size(200, 72);
+            this.CadastrarItem.TabIndex = 0;
+            this.CadastrarItem.Text = "Cadastrar Item";
+            this.CadastrarItem.UseVisualStyleBackColor = false;
+            // 
+            // CadastrarComodo
+            // 
+            this.CadastrarComodo.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.CadastrarComodo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CadastrarComodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CadastrarComodo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CadastrarComodo.Location = new System.Drawing.Point(219, 15);
+            this.CadastrarComodo.Name = "CadastrarComodo";
+            this.CadastrarComodo.Size = new System.Drawing.Size(200, 72);
+            this.CadastrarComodo.TabIndex = 1;
+            this.CadastrarComodo.Text = "Cadastrar Cômodo";
+            this.CadastrarComodo.UseVisualStyleBackColor = false;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 108);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Itens";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(60, 124);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(327, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cômodos";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(665, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(309, 268);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // SRLOC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(986, 476);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "SRLOC";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo de Requísitos";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,8 +165,13 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CadastrarComodo;
+        private System.Windows.Forms.Button CadastrarItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
